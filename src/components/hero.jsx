4da2083e8dcf2b87661280f2db5113cedc9faa28
daoatzy <dafasetiadi65@gmail.com>
2023-10-29 09:01:22 +0700

    update content

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -5,11 +5,11 @@ export default function Hero(){
         <>
         <div className="container">
     <div className="hero h-96 md:h-[500px] rounded-box overflow-hidden" style={{backgroundImage: 'url(https://wallpapers.com/images/featured/jdm-aesthetic-yggqyl2azy9d5pqg.jpg)'}}>
-  <div className="hero-overlay bg-opacity-50"></div>
+  <div className="hero-overlay bg-opacity-40"></div>
   <div className="hero-content text-center text-neutral-content">
     <div className="max-w-lg">
       <h1 className="mb-5 sm:mb-7 text-4xl sm:text-5xl font-bold">Hero</h1>
-      <p className="mb-5 sm:mb-7 sm:text-lg">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+      <p className="mb-5 sm:mb-7 sm:text-lg">Hero is a component for displaying a large box or image with a title and description.</p>
       <button className="btn btn-ghost sm:btn-wide">Get Started</button>
     </div>
   </div>
--- src/components/services.jsx
+++ src/components/services.jsx
@@ -6,62 +6,67 @@ export default function Services(){
         <section className='py-10 md:py16'>
             <div className='container'>
                 <div className='text-center'>
-                    <h2 className='text-3xl sm:5xl font-bold mb-4'>What We do</h2>
-                    <p className='text-lg sm:text-2xl mb-6 md:mb-14'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, facilis eligendi. Similique eius porro omnis tenetur optio magni dolores! Facilis vero ad nobis id optio quos eum sapiente maiores pariatur.</p>
+                    <h2 className='text-3xl sm:5xl font-bold mb-4'>Content Section</h2>
+                    <p className='text-lg sm:text-2xl mb-6 md:mb-14'>After adding the header and hero to the website, usually after that part has been added, the next part of the website is the content section which functions as a place where we put whatever content we want that matches the theme of the website. </p>
                 </div>
             </div>
         </section>
 
         <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 lg:gap-8 xl:gap-10'>
             <div className='card bg-base-200 transform-gpu transition-all duration-300 hover:-translate-y-2 hover:shadow-lg'>
+            <img src="https://rare-gallery.com/livewalls/imgpreview/146981-yellow-car-driving-along-the-city-2k.jpg" className="w-full h-auto rounded-xl" />
                 <div className="card-body items-center text-center gap-4 ">
-                    <h2 className="card-title">maklo</h2>
-                    <p>lorem <br className="hidden xl:inline" />
-                    ipsum<br className="hidden xl:inline" />masbro</p>
+                    <h2 className="card-title">Content</h2>
+                    <p>This <br className="hidden xl:inline" />
+                    Is <br className="hidden xl:inline" /> Content</p>
 
                 </div>
             </div>
             <div className='card bg-base-200 transform-gpu transition-all duration-300 hover:-translate-y-2 hover:shadow-lg'>
+            <img src="https://rare-gallery.com/livewalls/imgpreview/146981-yellow-car-driving-along-the-city-2k.jpg" className="w-full h-auto rounded-xl" />
                 <div className="card-body items-center text-center gap-4 ">
-                    <h2 className="card-title">maklo</h2>
-                    <p>lorem <br className="hidden xl:inline" />
-                    ipsum<br className="hidden xl:inline" />masbro</p>
+                    <h2 className="card-title">Content</h2>
+                    <p>This <br className="hidden xl:inline" />
+                    Is <br className="hidden xl:inline" /> Content</p>
 
                 </div>
             </div>
             <div className='card bg-base-200 transform-gpu transition-all duration-300 hover:-translate-y-2 hover:shadow-lg'>
+            <img src="https://rare-gallery.com/livewalls/imgpreview/146981-yellow-car-driving-along-the-city-2k.jpg" className="w-full h-auto rounded-xl" />
                 <div className="card-body items-center text-center gap-4 ">
-                    <h2 className="card-title">maklo</h2>
-                    <p>lorem <br className="hidden xl:inline" />
-                    ipsum<br className="hidden xl:inline" />masbro</p>
+                    <h2 className="card-title">Content</h2>
+                    <p>This <br className="hidden xl:inline" />
+                    Is <br className="hidden xl:inline" /> Content</p>
 
                 </div>
             </div>
             <div className='card bg-base-200 transform-gpu transition-all duration-300 hover:-translate-y-2 hover:shadow-lg'>
+            <img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiLBYRYkMhqoihtwzZtmZf1mcFHFT7hUyFEzEQS2mknZnb_XkKVFxKxkKA-5rcmiRDmcaaHRSBa3X0vPXteBAZ604UptQl0H1dohy6kEIgdJl1ngXwRgIPYFm4Hm4_D_vfX9qu5TYytBm-_yPIt3IC74tSP3NoFGwgkufdW6PJsDsE53O-xoSx0jHQJ3A/s3840/WHITE-FERRARI-HEROSCREEN-5192023.png" className="w-full h-auto rounded-xl" />
                 <div className="card-body items-center text-center gap-4 ">
-                    <h2 className="card-title">maklo</h2>
-                    <p>lorem <br className="hidden xl:inline" />
-                    ipsum<br className="hidden xl:inline" />masbro</p>
+                    <h2 className="card-title">Content</h2>
+                    <p>This <br className="hidden xl:inline" />
+                    Is <br className="hidden xl:inline" /> Content</p>
 
                 </div>
             </div>
             <div className='card bg-base-200 transform-gpu transition-all duration-300 hover:-translate-y-2 hover:shadow-lg'>
+            <img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiLBYRYkMhqoihtwzZtmZf1mcFHFT7hUyFEzEQS2mknZnb_XkKVFxKxkKA-5rcmiRDmcaaHRSBa3X0vPXteBAZ604UptQl0H1dohy6kEIgdJl1ngXwRgIPYFm4Hm4_D_vfX9qu5TYytBm-_yPIt3IC74tSP3NoFGwgkufdW6PJsDsE53O-xoSx0jHQJ3A/s3840/WHITE-FERRARI-HEROSCREEN-5192023.png" className="w-full h-auto rounded-xl" />
                 <div className="card-body items-center text-center gap-4 ">
-                    <h2 className="card-title">maklo</h2>
-                    <p>lorem <br className="hidden xl:inline" />
-                    ipsum<br className="hidden xl:inline" />masbro</p>
+                    <h2 className="card-title">Content</h2>
+                    <p>This <br className="hidden xl:inline" />
+                    Is <br className="hidden xl:inline" /> Content</p>
 
                 </div>
             </div>
-            <div className='card bg-base-200 transform-gpu transition-all duration-300 hover:-translate-y-2 hover:shadow-lg'>
+             <div className='card bg-base-200 transform-gpu transition-all duration-300 hover:-translate-y-2 hover:shadow-lg'>
+             <img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiLBYRYkMhqoihtwzZtmZf1mcFHFT7hUyFEzEQS2mknZnb_XkKVFxKxkKA-5rcmiRDmcaaHRSBa3X0vPXteBAZ604UptQl0H1dohy6kEIgdJl1ngXwRgIPYFm4Hm4_D_vfX9qu5TYytBm-_yPIt3IC74tSP3NoFGwgkufdW6PJsDsE53O-xoSx0jHQJ3A/s3840/WHITE-FERRARI-HEROSCREEN-5192023.png" className="w-full h-auto rounded-xl" />
                 <div className="card-body items-center text-center gap-4 ">
-                    <h2 className="card-title">maklo</h2>
-                    <p>lorem <br className="hidden xl:inline" />
-                    ipsum<br className="hidden xl:inline" />masbro</p>
+                    <h2 className="card-title">Content</h2>
+                    <p>This <br className="hidden xl:inline" />
+                    Is <br className="hidden xl:inline" /> Content</p>
 
                 </div>
             </div>
-            
         </div>
         
         </>
